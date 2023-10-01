941bc9bbae977af2ec23f5c3caff74463725e76c
Luc Vo <luke.syvo@gmail.com>
2023-10-01 12:39:27 -0600

    create postSlice, friendSlice, userSlice, commentSlice. Update profile page, create profileAbout, ProfileScoreCard, pofileSocialinfo

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,15 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import commentReducer from "../features/comment/commentSlice";
+import friendReducer from "../features/friend/friendSlice";
+import postReducer from "../features/post/postSlice";
+import userReducer from "../features/user/userSlice";
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+  comment: commentReducer,
+  friend: friendReducer,
+  post: postReducer,
+  user: userReducer
+});
 
 const store = configureStore({
   reducer: rootReducer
