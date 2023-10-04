d566adac2b736b47f3e8bc3d2756e6e6b8bed717
Luc Vo <luke.syvo@gmail.com>
2023-10-03 22:16:20 -0600

    update PostList

--- src/features/post/PostForm.js
+++ src/features/post/PostForm.js
@@ -1,7 +1,81 @@
 import React from "react";
+import { FormProvider, FTextField } from "../../components/form";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as Yup from "yup";
+import { alpha, Box, Card, Stack } from "@mui/material";
+import { LoadingButton } from "@mui/lab";
+import { useDispatch } from "react-redux";
+import { createPost } from "./postSlice";
+
+const yupSchema = Yup.object().shape({
+  content: Yup.string().required("Content is required")
+});
+
+const defaultValues = {
+  content: "",
+  image: ""
+};
 
 function PostForm() {
-  return <div>PostForm</div>;
+  const methods = useForm({
+    resolver: yupResolver(yupSchema),
+    defaultValues
+  });
+
+  const {
+    handleSubmit,
+    reset,
+    setValue,
+    formState: { isSubmitting }
+  } = methods;
+
+  const dispatch = useDispatch();
+
+  const onSubmit = (data) => {
+    dispatch(createPost(data)).then(() => reset());
+  };
+
+  return (
+    <Card sx={{ p: 3 }}>
+      <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+        <Stack spacing={2}>
+          <FTextField
+            name="content"
+            multiline
+            fullWidth
+            rows={4}
+            placeholder="Share what you are thinking here..."
+            sx={{
+              "& fieldset": {
+                borderWidth: `1px !important`,
+                borderColor: alpha("#919EAB", 0.32)
+              }
+            }}
+          />
+
+          <FTextField name="image" />
+
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "flex-end"
+            }}
+          >
+            <LoadingButton
+              type="submit"
+              variant="contained"
+              size="small"
+              loading={isSubmitting}
+            >
+              Post
+            </LoadingButton>
+          </Box>
+        </Stack>
+      </FormProvider>
+    </Card>
+  );
 }
 
 export default PostForm;
--- src/features/post/postSlice.js
+++ src/features/post/postSlice.js
@@ -1,15 +1,74 @@
 import { createSlice } from "@reduxjs/toolkit";
 import apiService from "../../app/apiService";
+import { POSTS_PER_PAGE } from "../../app/config";
 
 const initialState = {
   isLoading: false,
-  error: null
+  error: null,
+  postById: {},
+  currentPagePosts: []
 };
 
 const slice = createSlice({
   name: "post",
   initialState,
-  reducers: {}
+  reducers: {
+    startLoading(state) {
+      state.isLoading = true;
+    },
+    hasError(state, action) {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
+    createPostSuccess(state, action) {
+      state.isLoading = false;
+      state.error = null;
+      const newPost = action.payload;
+      if (state.currentPagePosts.length % POSTS_PER_PAGE === 0)
+        state.currentPagePosts.pop();
+      state.postsById[newPost._id] = newPost;
+      state.currentPagePosts.unshift(newPost._id);
+    },
+    getPostsSuccess(state, action) {
+      state.isLoading = false;
+      state.error = null;
+      const { count, posts } = action.payload;
+      posts.forEach((post) => {
+        state.postById[post._id] = post;
+        if (!state.currentPagePosts.includes(post._id)) {
+          state.currentPagePosts.push(post._id);
+        }
+      });
+      state.totalPosts = count;
+    }
+  }
 });
+
+export const createPost =
+  ({ content, image }) =>
+  async (dispatch) => {
+    dispatch(slice.actions.startLoading());
+    try {
+      const response = await apiService.post("/posts", { content, image });
+      dispatch(slice.actions.createPostSuccess(response.data));
+    } catch (error) {
+      dispatch(slice.actions.hasError(error));
+    }
+  };
+
+export const getPosts =
+  ({ userId, page, limit = 2 }) =>
+  async (dispatch) => {
+    dispatch(slice.actions.startLoading());
+    try {
+      const params = { page, limit };
+      const response = await apiService.get(`/posts/user/${userId}`, {
+        params
+      });
+      dispatch(slice.actions.getPostsSuccess(response.data));
+    } catch (error) {
+      dispatch(slice.actions.hasError(error));
+    }
+  };
 
 export default slice.reducer;
