ac0fd9191f6bed2ecde25ff25400c4582787f8b9
Luc Vo <luke.syvo@gmail.com>
2023-10-04 21:07:04 -0600

    create PostReaction

--- src/features/post/PostReaction.js
+++ src/features/post/PostReaction.js
@@ -1,7 +1,30 @@
+import { IconButton, Stack, Typography } from "@mui/material";
 import React from "react";
+import ThumbDownAltRoundedIcon from "@mui/icons-material/ThumbDownAltRounded";
+import ThumbUpRoundedIcon from "@mui/icons-material/ThumbUpRounded";
 
 function PostReaction({ post }) {
-  return <div>PostReaction</div>;
+  const handleClick = (emoji) => {
+    console.log(emoji);
+  };
+
+  return (
+    <Stack direction="row" aligItems="center">
+      <IconButton onClick={() => handleClick("like")}>
+        <ThumbUpRoundedIcon sx={{ fontSize: 20, color: "primary.main" }} />
+      </IconButton>
+      <Typography variant="h6" mr={1}>
+        {post?.reactions?.like}
+      </Typography>
+
+      <IconButton onClick={() => handleClick("like")}>
+        <ThumbDownAltRoundedIcon sx={{ fontSize: 20, color: "primary.main" }} />
+      </IconButton>
+      <Typography variant="h6" mr={1}>
+        {post?.reactions?.dislike}
+      </Typography>
+    </Stack>
+  );
 }
 
 export default PostReaction;
